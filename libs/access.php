a2b1e0481c0bce0899289ae19e0e3eacb34075ba
matius54 <mat54200454@gmail.com>
2024-04-10 02:08:32 -0400

    Building Node class

--- libs/access.php
+++ libs/access.php
@@ -1,45 +1,88 @@
 <?php
     require_once "db.php";
+
     class Access {
-        private static string $access = "access.json";
-
-        private static string $key = "k";
-        private static string $name = "n";
-        private static string $description = "d";
-
-        private array $data;
 
         public function __construct() {
-            $this->data = json_decode(
-                json: file_get_contents(self::$access),
-                associative: true
-            );
+            
         }
-        public function getAllAccess() : array {
-            return $this->data;
-        }
+
         public function toHtml() : string {
             return "";
         }
+
         public function unsetRoleKey(string $role ,string $key) : void {
 
         }
+
         public function hasAccess(string $role ,string $key) : bool|null {
             //buscar una key especifica en la base de datos
             return null;
         }
     }
-    class Role {
-        private string $key;
-        public function __construct(int $role_id) {
-        }
-    }
+
     class Node {
-        public function __construct(string $key) {
-            $sql = "SELECT ";
-        
+        private static string $filename = "nodes.json";
+
+        private ?string $key = null;
+        private ?string $name = null;
+        private ?string $description = null;
+
+        private array $all = [];
+
+        public function __construct(string $key = "") {
+            $this->all = self::getAll();
+            $this->setKey($key);
+        }
+
+        public static function getAll() : array {
+            return json_decode(
+                json: file_get_contents(self::$filename),
+                associative: true
+            ) ?? [];
+        }
+
+        public function get() : array|null {
+            if(isset($this->key)){
+                return [
+                    "key" => $this->key,
+                    "name" => $this->name,
+                    "description" => $this->description
+                ];
+            }
+            return null;
+        }
+
+        public function getKey() : string {
+            return $this->key;
+        }
+
+        public function getName() : string {
+            return $this->name;
+        }
+
+        public function getDescription() : string {
+            return $this->description;
+        }
+
+        public function setKey(string $key = "") : void {
+            if($node = $this->all[$key] ?? ""){
+                $this->key = $key;
+                $this->name = $node["name"];
+                $this->description = $node["description"]; 
+            }
         }
     }
-    $acc = new Access;
-    var_dump($acc->getAllAccess());
+
+    class Role {
+        public function __construct(int $role_id) {
+            $sql = "SELECT ";
+        }
+
+        public static function getAll(){
+
+        }
+    }
+
+    var_dump(Node::getAll());
 ?>

9e5782701c583bb35148b9a629d659cd31f59a7d
matius54 <mat54200454@gmail.com>
2024-04-30 13:17:02 -0400

    dashboard.html reemplazado por dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,65 +1,42 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <style>
-:root {
-    --main-color: black;
-}
-* {
-    color-scheme: dark;
-}
-body {
-    background-color: var(--main-color);
-    display: grid;
-    margin:0;
-    padding:0;
-    width: 100%;
-    height: 100vh;
-    font-family: sans-serif;
-    grid-template-columns: min-content auto;
-    grid-template-rows: 80px auto;
-}
-header {
-    margin: 0;
-}
-.frame {
-    height: 100%;
-    width: 100%;
-}
-ul {
-    display: inline;
-}
-    </style>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-<body>
-    <div>
+<?php
+    $title = "test";
+    $base = "php/";
+    require_once "php/model/user.php";
+    require_once "php/libs/template.php";
 
-    </div>
-
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#">Pagina1</a></li>
-                <li><a href="#">Pagina2</a></li>
-                <input id="color" type="color" onchange="updateColor()">
-            </ul>
-        </nav>
-    </header>
-    <div>
-        
-    </div>
-    <main>
-        
-    </main>
-</body>
-<script>
-    let color = document.getElementById("color")
-    function updateColor(){
-        console.log(color.value)
-        document.documentElement.style.setProperty('--main-color',color.value);
-    }
-</script>
-</html>
+    $user_id = User::verify();
+    
+    Template::render("header",["title" => $title]);
+    Template::render("navbar",[
+        "items" => [
+            [
+                "name" => "Registros",
+                "icon" => "record",
+                "href" => "#"
+            ],
+            [
+                "name" => "Mi perfil",
+                "icon" => "profile",
+                "href" => "#"
+            ],
+            [
+                "name" => "Roles",
+                "icon" => "edit",
+                "href" => "./role.html"
+            ],
+            [
+                "name" => "Usuarios",
+                "icon" => "profile2",
+                "href" => "#"
+            ],
+            [
+                "name" => "Cerrar sesion",
+                "icon" => "logout",
+                "href" => "#",
+                "last" => true
+            ],
+        ]
+    ]);
+    Template::render("nav",["title" => $title]);
+    Template::render("footer");
+?>

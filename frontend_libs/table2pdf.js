bd7f9a3b5d00c1c6952834915f60df19da726524
matius54 <mat54200454@gmail.com>
2024-05-07 19:22:53 -0400

    subo cambios para el descargador de tablas

--- frontend_libs/table2pdf.js
+++ frontend_libs/table2pdf.js
@@ -1,8 +1,10 @@
 if (!window.jsPDF) window.jsPDF = window.jspdf.jsPDF;
 
 document.querySelectorAll("table").forEach(table => {
+    if(table.innerHTML === "<tbody></tbody>") return;
     const downloadBtt = document.createElement("button");
     downloadBtt.innerText = "Descargar tabla";
+    downloadBtt.classList.add("button");
     downloadBtt.addEventListener("click", ()=>{
         const doc = new jsPDF();
         doc.autoTable({ html: table });

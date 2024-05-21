27c30beb7a7270eae7aaba4848dba5fa9af9939e
Matius54 <mat54200454@gmail.com>
2024-05-21 19:04:12 -0400

    modificado enlace de github para que no apunte a un commit especifico

--- components/nav.php
+++ components/nav.php
@@ -13,6 +13,6 @@
     </div>
     <h1><?= $title ?? "Titulo" ?></h1>
     <div class="options right">
-        <a class="button small" target="_blank" href="https://github.com/matius54/el_proyecto_php/tree/31a1a2f0a11724aac4021bc0654ceda10d71e26d">Ver en GitHub</a>
+        <a class="button small" target="_blank" href="https://github.com/matius54/el_proyecto_php/">Ver en GitHub</a>
     </div>
 </header>

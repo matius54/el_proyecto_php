ef4079c44f0f972b00b85d405f2688badfdea86a
Matius54 <mat54200454@gmail.com>
2024-05-24 10:06:51 -0400

    consulta añadido otro join

--- php/model/reports.php
+++ php/model/reports.php
@@ -13,7 +13,7 @@
             foreach($roles as $role){
                 $id = $role["id"];
                 $name = $role["name"];
-                $this->reports["Reporte de $name"] = "SELECT u.user, u.ci, r.name AS role FROM `user` AS u JOIN `user_role` AS ru ON u.id = user_id JOIN `role` AS r ON ru.role_id = r.id WHERE r.id = $id";
+                $this->reports["Reporte de $name"] = "SELECT u.id, u.user, u.ci, r.name, e.is_exit, e.created_at FROM `user` AS u JOIN `user_role` AS ru ON u.id = user_id JOIN `role` AS r ON ru.role_id = r.id JOIN `event` AS e ON u.id = e.user_id WHERE r.id = $id";
             }
         }
 
